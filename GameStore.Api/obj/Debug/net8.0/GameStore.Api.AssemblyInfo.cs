da7217fb18340d0d288d88c7be6014e4972d5692
Kenny Jinhiro <jxnhiro@gmail.com>
2024-01-07 15:30:27 +0700

    Finished Basic Rest API

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922b7c25682b6c658f9c2d2e1707acd781e7fd08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e79a35bbee3aaefd7a2d41052597755ac18044c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
